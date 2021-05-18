1340d0c95d33e6d64d225575c3a33e62c4a1f48e
Chinmay Aggarwal <chinmay@digishaala.com>
2021-05-18 12:47:21 +0530

    starting with infrastructure

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Exceptions
+{
+    public class ValidationException : ApplicationException
+    {
+        public ValidationException()
+            : base("One or more validations failures have occured.")
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failiures)
+            : this()
+        {
+            Errors = failiures
+                .GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+                .ToDictionary(failiureGroup => failiureGroup.Key, failiureGroup => failiureGroup.ToArray());
+        }
+
+        public IDictionary<string, string[]> Errors { get; }
+    }
+}
